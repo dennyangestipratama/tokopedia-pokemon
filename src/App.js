a315dbd58cdec9e103076d2b41817c17a4fb0265
dennyangesti <dennyangesti@gmail.com>
2020-12-17 19:36:43 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
-function App() {
+const App = () => {
+  const client = new ApolloClient({
+    uri: "https://graphql-pokeapi.vercel.app/api/graphql",
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <main>
+        <p>I'm a Pokemon</p>
+      </main>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
